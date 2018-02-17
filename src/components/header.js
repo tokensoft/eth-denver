2e7e5e0bba9866d4bd06e93916d270540f130c8f
Ron Gierlach <rongierlach@gmail.com>
2018-02-16 23:53:10 -0800

    use next/link in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,18 @@
+import Link from 'next/link'
 import Container from './container'
 
 const Header = () => (
   <header className='pv4 z-999'>
     <Container className='flex flex-row items-center justify-between'>
-      <a href='#' className='link near-black f3 fw6 flex flex-row items-center' to='/'>
-        <span>Etheregg</span>
-        <img
-          className='dib h2 pl2'
-          src='/static/logo.svg'
-        />
-      </a>
+      <Link href='/'>
+        <a className='link pointer near-black hover-near-black f3 fw6 flex flex-row items-center'>
+          <span>Etheregg</span>
+          <img
+            className='dib h2 pl2'
+            src='/static/logo.svg'
+          />
+        </a>
+      </Link>
       <div className='tr'>
         <div>12.45 ETH</div>
         <div className='f7'>
