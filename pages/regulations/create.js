e62c13ea277e4da4ad9d3cc956f1c5dbaa0d2259
Ron Gierlach <rongierlach@gmail.com>
2018-02-17 07:23:52 -0800

    move some things around

---
+++ pages/regulations/create.js
@@ -0,0 +1,12 @@
+// import Link from 'next/link'
+import Layout from '../../src/layouts'
+import Head from '../../src/components/head'
+import RegulatorServiceForm from '../../src/components/forms/regulator-service.js'
+
+export default () => (
+  <Layout>
+    <div className='center measure'>
+      <RegulatorServiceForm />
+    </div>
+  </Layout>
+)
--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,24 @@ import Link from 'next/link'
 import Container from './container'
 
 const Header = () => (
-  <header className='pv4 z-999'>
+  <header className='pv4 z-1'>
     <Container className='flex flex-row items-center justify-between'>
-      <Link href='/'>
-        <a className='link pointer near-black hover-near-black f3 fw6 flex flex-row items-center'>
-          <span>Etheregg</span>
-          <img
-            className='dib h2 pl2'
-            src='/static/logo.svg'
-          />
-        </a>
-      </Link>
+      <div className='flex flex-column'>
+        <Link href='/'>
+          <a className='link pointer near-black hover-near-black f3 fw6 flex flex-row items-center'>
+            <span>Etheregg</span>
+            <img
+              className='dib h2 pl2'
+              src='/static/logo.svg'
+            />
+          </a>
+        </Link>
+        <div className='flex flex-row z-999'>
+          <Link href='/regulations'><a className='link near-black underline-hover'>Regulations</a></Link>
+          <span>&nbsp;|&nbsp;</span>
+          <Link href='/tokens'><a className='link near-black underline-hover'>R-Tokens</a></Link>
+        </div>
+      </div>
       <div className='tr'>
         <div>12.45 ETH</div>
         <div className='f7'>
