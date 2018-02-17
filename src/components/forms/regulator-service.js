a750a8e4b08a2876475ca70325e0a9e973bdc055
Ron Gierlach <rongierlach@gmail.com>
2018-02-17 07:26:08 -0800

    stub states field

--- src/components/forms/regulator-service.js
+++ src/components/forms/regulator-service.js
@@ -1,13 +1,21 @@
 import { Form, Checkbox, Divider, Grid, Button } from 'semantic-ui-react'
 import withSemanticUIFormik from '../hocs/withSemanticUIFormik'
+import states from '../../../static/us-states'
+
 const countries = require('country-list')()
 
+const __COUNTRY__ = 'US'
 const countryCodes = countries.getCodes()
 const countryNamesByCode = countries.getCodeList()
-const countryOptions = countryCodes.map(countryCode => ({
+const countryOptions = [{ text: 'All', value: '' }].concat(countryCodes.map(countryCode => ({
   text: countryNamesByCode[countryCode.toLowerCase()],
   value: countryCode
-}))
+})))
+
+const stateOptions = [{ text: 'All', value: '' }].concat(Object.keys(states).map(stateCode => ({
+  text: states[stateCode],
+  value: stateCode
+})))
 
 const RegulatorServiceForm = props => {
   const {
@@ -40,13 +48,27 @@ const RegulatorServiceForm = props => {
       <h4 className='mb0'>Restrictions</h4>
       <Divider />
       <Grid>
-        <Grid.Column computer={8} className='mb3'>
+        <Grid.Column computer={8}>
           <Form.Input
+            required
             onChange={handleChange}
-            label="Age" name="age" defaultValue={18} min={18} type="number"
+            label="Min Age" name="minAge" defaultValue={16} min={16} type="number"
           />
         </Grid.Column>
         <Grid.Column computer={8}>
+          <Form.Input
+            onChange={handleChange}
+            label="Max Age" name="maxAge" type="number" placeholder={'N/A'}
+          />
+        </Grid.Column>
+        <Grid.Column computer={8} className='mb3'>
+          <Form.Input
+            required
+            onChange={handleChange}
+            label="Min Investors" name="minInvestors" type="number" min={1} defaultValue={1}
+          />
+        </Grid.Column>
+        <Grid.Column computer={8} className='mb3'>
           <Form.Input
             onChange={handleChange}
             label="Max Investors" name="maxInvestors" type="number" min={1} placeholder={'Unlimited'}
@@ -54,16 +76,32 @@ const RegulatorServiceForm = props => {
         </Grid.Column>
       </Grid>
       <Form.Dropdown
+        name='countries'
         search
         selection
+        required
         multiple
         allowAdditions
-        label={'Countries (allowed)'}
+        label={'Countries'}
         defaultValue={['US']}
-        additionLabel='Include'
+        additionLabel='Add'
         options={countryOptions}
         // onAddItem
       />
+      { __COUNTRY__ === 'US' && <Form.Dropdown
+          name='states'
+          search
+          selection
+          required
+          multiple
+          allowAdditions
+          label={'States'}
+          defaultValue={['']}
+          additionLabel='Add'
+          options={stateOptions}
+          // onAddItem
+        />
+      }
       <Checkbox
         className='mb3'
         onChange={handleChange}
