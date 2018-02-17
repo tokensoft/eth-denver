3462d551aa7c2ad2877e2c9ec7f25c0ca10745e6
Ron Gierlach <rongierlach@gmail.com>
2018-02-17 08:17:42 -0800

    create token listview

---
+++ pages/tokens/create.js
@@ -0,0 +1,10 @@
+import Layout from '../../src/layouts'
+import RegulatedTokenForm from '../../src/components/forms/regulated-token'
+
+export default () => (
+  <Layout>
+    <div className='center measure'>
+      <RegulatedTokenForm />
+    </div>
+  </Layout>
+)
