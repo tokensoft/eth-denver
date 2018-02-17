a80361aca4d5623d4b5c15f26f3e20130d175269
Ron Gierlach <rongierlach@gmail.com>
2018-02-17 08:14:23 -0800

    fix broken links

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,13 @@ export default () => (
         <div className='near-black tr-ns'>
           <h2 className='fw6'>For Investors</h2>
           <Link href='/verify-investor-status'><a className='db link dark-gray no-underline underline-hover f6'>Verify investor status &#8594;</a></Link>
-          <Link href='/discover-regulated-tokens'><a className='db link dark-gray no-underline underline-hover f6'>Discover regulated tokens &#8594;</a></Link>
+          <Link href='/tokens'><a className='db link dark-gray no-underline underline-hover f6'>Discover regulated tokens &#8594;</a></Link>
         </div>
         <div className='bl bw1 mh4 h4-ns' />
         <div className='near-black tl-ns'>
           <h2 className='fw6'>For Regulators</h2>
           <Link href='/regulations/create'><a className='db link dark-gray no-underline underline-hover f6'>Create a regulator service &#8594;</a></Link>
-          <Link href='/deploy-regulated-token'><a className='db link dark-gray no-underline underline-hover f6'>Deploy a regulated token &#8594;</a></Link>
+          <Link href='/tokens/create'><a className='db link dark-gray no-underline underline-hover f6'>Deploy a regulated token &#8594;</a></Link>
         </div>
       </div>
     </div>
