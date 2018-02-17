edce54b3123108c44811a7e59708b7b26076559d
Ron Gierlach <rongierlach@gmail.com>
2018-02-17 08:14:44 -0800

    lint

--- pages/regulations/create.js
+++ pages/regulations/create.js
@@ -1,6 +1,5 @@
 // import Link from 'next/link'
 import Layout from '../../src/layouts'
-import Head from '../../src/components/head'
 import RegulatorServiceForm from '../../src/components/forms/regulator-service.js'
 
 export default () => (
