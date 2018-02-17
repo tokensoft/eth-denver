77a5b126a2dc51a4e29c1197429d89d3627919ef
Ron Gierlach <rongierlach@gmail.com>
2018-02-16 22:04:50 -0800

    stub index page

--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,24 @@
-import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
+// import Link from 'next/link'
+import Layout from '../src/layouts'
+import Head from '../src/components/head'
 
 export default () => (
-  <div>
-    <Head title="Home" />
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next!</h1>
-      <p className="description">To get started, edit <code>pages/index.js</code> and save to reload.</p>
-
-      <div className="row">
-        <Link href="https://github.com/zeit/next.js#getting-started">
-          <a className="card">
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next on Github and in their examples</p>
-          </a>
-        </Link>
-        <Link href="https://open.segment.com/create-next-app">
-          <a className="card">
-            <h3>Examples &rarr;</h3>
-            <p>
-              Find other example boilerplates on the <code>create-next-app</code> site
-            </p>
-          </a>
-        </Link>
-        <Link href="https://github.com/segmentio/create-next-app">
-          <a className="card">
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it</p>
-          </a>
-        </Link>
+  <Layout>
+    <Head />
+    <div className='absolute top-0 left-0 w-100 vh-100 flex flex-column justify-center align-center'>
+      <div className='center flex flex-column flex-row-ns align-center justify-center-ns'>
+        <div className='near-black tr-ns'>
+          <h2 className='fw6'>For Investors</h2>
+          <a href='#' className='db link dark-gray no-underline underline-hover f6'>Verify investor status &#8594;</a>
+          <a href='#' className='db link dark-gray no-underline underline-hover f6'>Discover regulated tokens &#8594;</a>
+        </div>
+        <div className='bl bw1 mh4 h4-ns' />
+        <div className='near-black tl-ns'>
+          <h2 className='fw6'>For Regulators</h2>
+          <a href='#' className='db link dark-gray no-underline underline-hover f6'>Create a regulator service &#8594;</a>
+          <a href='#' className='db link dark-gray no-underline underline-hover f6'>Deploy a regulated token &#8594;</a>
+        </div>
       </div>
     </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title, .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9B9B9B;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
+  </Layout>
 )
